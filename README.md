a5b85eebf357a1e6c4d205a429cb58e0feb101be
Tejasvi T <tejasvi.thota@transformingsystems.com>
2020-06-04 19:22:43 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PiGPIO
 Pi Zero GPIO Testing
+
+This is Golang samples repository for self evalation and testing purposes only.
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "log"
+
+func main() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
+	log.Println("Hello")
+}
